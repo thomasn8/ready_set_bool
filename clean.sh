bc6793ea7b6417e2fcbdf0a7ead569977824c267
Thomas Nanchen <thomasnanchen@hotmail.com>
2024-03-09 15:47:36 +0100

    initial commit

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Iterate over directories starting with 'ex0'
+for dir in ex0*; do
+    if [ -d "$dir" ]; then  # Check if it's a directory
+        echo "Cleaning $dir"
+        # Delete binary files
+        find "$dir" -maxdepth 1 -type f -perm +u=x -delete
+        # Delete object files
+        find "$dir" -maxdepth 1 -type f -name "*.o" -delete
+    fi
+done
